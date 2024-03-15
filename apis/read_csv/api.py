4ef16a1c9188da8cc29880840eab3d26f86351c8
yen <oe327188@gmail.com>
2024-03-16 00:17:24 +0800

    update read_csv.

--- apis/read_csv/api.py
+++ apis/read_csv/api.py
@@ -3,16 +3,21 @@ from kernel.task import TaskResult, TaskExecResult
 import os
 
 def api(args:dict)->TaskResult:
-    # os.system("which python")
+
     task_result = TaskResult()
-    count = args.get('count',False)
-    if isinstance(count,bool):
+    start_index = args.get('start',False)
+    if isinstance(start_index,bool):
         task_result.exec_result = TaskExecResult.FAIL
         return  task_result
+    end_index = args.get('end',False)
+    if isinstance(end_index,bool):
+        task_result.exec_result = TaskExecResult.FAIL
+        return  task_result
+    
     csv_path = "/home/mingyen/Desktop/SideProject/Toolformer/test_maintenance.csv"
     df = pd.read_csv(csv_path)
     # 篩選出指定範圍的 UDI
-    df_filtered = df.head(n=count)
+    df_filtered = df[df['UDI'].between(start_index, end_index)]
     
     task_result.val = df_filtered
     task_result.exec_result = TaskExecResult.SUCCESS

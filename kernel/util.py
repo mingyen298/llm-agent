58451be3681058a3cce38c34d3c1fea71b9d5621
yen <oe327188@gmail.com>
2024-03-16 08:09:54 +0800

    ignore draw bar,line,pie chart api document

--- kernel/util.py
+++ kernel/util.py
@@ -11,12 +11,15 @@ class Util:
             for dir_name in dirnames:
                 if dir_name == 'test':
                     continue
+                try:
+                    with open(os.path.join(apis_path, dir_name, 'document.json'), 'r') as f:
+                        # content = f.read()
+                        content = json.load(f)
+                        doc_list.append(content)
+                    
+                except:
+                    continue
                 print(dir_name)
-                with open(os.path.join(apis_path, dir_name, 'document.json'), 'r') as f:
-                    # content = f.read()
-                    content = json.load(f)
-                    doc_list.append(content)
-
         with open(out_path, 'w') as f:
             json.dump(obj=doc_list, fp=f)
 
